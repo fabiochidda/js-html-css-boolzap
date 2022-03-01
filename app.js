4ea5d344bc3796dfc2b3dacf9eab9e46759f1ef9
fabiochidda <fabio.chidda99@gmail.com>
2022-03-01 22:20:32 +0100

    Cambiata la lista dei contatti ed i messaggi con il v-for per visualizzarli quando attivi

--- app.js
+++ app.js
@@ -1,6 +1,91 @@
 const app = new Vue({
 
     el: "#app",
+    data: {
+        currentChat: 0,
+        contacts: [
+            {
+                name: "Franco",
+                avatar: "img/img-01.png",
+                visible: true,
+                messages: [
+                    {
+                        date: "10/01/2020 15:30:55",
+                        text: "Stasera facciamo il raid, vieni con il warlock",
+                        status: "received",
+                    },
+                    {
+                        date: "10/01/2020 15:30:55",
+                        text: "Perfetto",
+                        status: "sent",
+                    },
+                ]
+            },
+            {
+                name: "Maria",
+                avatar: "img/img-03.png",
+                visible: true,
+                messages: [
+                    {
+                        date: "15/03/2020 18:50:55",
+                        text: "Hai visto il nuovo le nuove puntate di AoT?",
+                        status: "sent",
+                    },
+                    {
+                        date: "15/03/2020 18:55:55",
+                        text: "No, me le vedo stasera",
+                        status: "received",
+                    },
+                ]
+            },
+            {
+                name: "Andrea",
+                avatar: "img/img-02.png",
+                visible: true,
+                messages: [
+                    {
+                        date: "11/02/2020 11:05:55",
+                        text: "Oggi andiamo a mangiare sushi, vieni?",
+                        status: "received",
+                    },
+                    {
+                        date: "11/02/2020 11:10:55",
+                        text: "Assolutamente si",
+                        status: "sent",
+                    },
+                    {
+                        date: "11/02/2020 11:25:55",
+                        text: "Daje",
+                        status: "received",
+                    },
+                ]
+            },
+            {
+                name: "Marco",
+                avatar: "img/img-01.png",
+                visible: true,
+                messages: [
+                    {
+                        date: "10/01/2020 15:30:55",
+                        text: "Oggi usciamo?",
+                        status: "received",
+                    },
+                    {
+                        date: "10/01/2020 15:35:55",
+                        text: "No",
+                        status: "sent",
+                    },
+                    {
+                        date: "10/01/2020 15:40:55",
+                        text: "Ok bro",
+                        status: "received",
+                    },
+                ]
+            },
+            
+        ]
+        
+    }
 
 
 
